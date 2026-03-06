9c5d2627be2fed869339d143916c9d936d6ae364
Pguerreroo <pguerrerogreenside@gmail.com>
2026-03-06 20:42:00 +0100

    Repositorios (SistemaInventario)

---
+++ SistemaInventario.AccesoDatos/Repositorio/IRepositorio/Interface1.cs
@@ -0,0 +1,37 @@
+﻿using SistemaInventario.AccesoDatos.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
+{
+    public interface IRepositorio<T> where T : class
+    {
+
+
+        T Obtener(int id);
+
+        IEnumerable<T>  ObtenerTodos(
+            Expression<Func<T,  bool>>  filtro =null,
+            string incluirPropiedades  = null,
+            bool isTracking  = true
+            );
+
+        T ObtenerPrimero(
+            Expression<Func<T, bool>> filtro =null,
+            string incluirPropiedades  = null,
+            bool isTracking = true
+            );
+
+        void Agregar(T entidad);
+
+        void Remover(T entidad);
+
+        void RemoverRango(IEnumerable<T> entidad);
+
+
+
+    }
+
+}
